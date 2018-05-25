7c64b8b9ee1e325194d015c15858d0d03bbbdcda
Ivan Kravets <me@ikravets.com>
2018-05-25 16:08:02 +0300

    Change Pygments style to pastie

--- conf.py
+++ conf.py
@@ -87,7 +87,7 @@ exclude_patterns = ['_build', '**/*_extra.rst']
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'pastie'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
