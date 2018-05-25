c81c85b861bd59e06978d37611ab68b3a54633da
Ivan Kravets <me@ikravets.com>
2018-05-25 15:31:09 +0300

    Revert back to 'sphinx' theme for Pygments

--- conf.py
+++ conf.py
@@ -87,7 +87,7 @@ exclude_patterns = ['_build', '**/*_extra.rst']
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'default'
+pygments_style = 'sphinx'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
