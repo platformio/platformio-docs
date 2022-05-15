bc40ca2298ad7223e532506fa5a8d4f086e80d7d
ivankravets <ivankravets@users.noreply.github.com>
2022-05-15 11:25:40 +0000

    Sync Docs platformio/platformio-core@575f0ae3008c3ae34795ced1311d72e244f4b5ba

--- en/latest/_static/documentation_options.js
+++ en/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.0.0rc2',
+    VERSION: '6.0.0rc3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
